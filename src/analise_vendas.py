8b357ae45e7e9de2ef6c9b3e7ed1180def1f2a13
Luis E. B. Canal <luiseduardocanal@gmail.com>
2026-01-05 11:02:21 -0300

    adiciona métricas básicas

--- src/analise_vendas.py
+++ src/analise_vendas.py
@@ -2,4 +2,12 @@ import pandas as pd
 
 df = pd.read_csv("data/vendas.csv")
 
-print(df.head())
+total_vendas = len(df)
+faturamento_total = df["preco"].sum()
+ticket_medio = df["preco"].mean()
+
+print("- MÉTRICAS GERAIS -")
+print("-------------------")
+print(f"Total de vendas: {total_vendas}")
+print(f"Faturamento total: {faturamento_total:,.2f}")
+print(f"Ticket Médio: {ticket_medio:,.2f}")
