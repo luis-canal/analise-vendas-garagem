199406619ad9a794cbb8a0c95a68a895648f46e4
Luis E. B. Canal <luiseduardocanal@gmail.com>
2026-01-05 18:42:22 -0300

    adiciona contagem de vendas por modelo

--- src/analise_vendas.py
+++ src/analise_vendas.py
@@ -5,9 +5,15 @@ df = pd.read_csv("data/vendas.csv")
 total_vendas = len(df)
 faturamento_total = df["preco"].sum()
 ticket_medio = df["preco"].mean()
+vendas_por_modelo = df["modelo"].value_counts()
+
 
 print("- MÉTRICAS GERAIS -")
 print("-------------------")
 print(f"Total de vendas: {total_vendas}")
 print(f"Faturamento total: {faturamento_total:,.2f}")
 print(f"Ticket Médio: {ticket_medio:,.2f}")
+
+print("\nVENDAS POR MODELO")
+print("-------------------")
+print(vendas_por_modelo)
