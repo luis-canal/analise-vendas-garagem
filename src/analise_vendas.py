9db27213ff25733ddc1952ca2d278fb3ee12dc71
Luis E. B. Canal <luiseduardocanal@gmail.com>
2026-01-05 18:47:11 -0300

    adiciona análise por forma de pagamento

--- src/analise_vendas.py
+++ src/analise_vendas.py
@@ -6,7 +6,7 @@ total_vendas = len(df)
 faturamento_total = df["preco"].sum()
 ticket_medio = df["preco"].mean()
 vendas_por_modelo = df["modelo"].value_counts()
-
+vendas_pagamento = df["forma_pagamento"].value_counts()
 
 print("- MÉTRICAS GERAIS -")
 print("-------------------")
@@ -17,3 +17,7 @@ print(f"Ticket Médio: {ticket_medio:,.2f}")
 print("\nVENDAS POR MODELO")
 print("-------------------")
 print(vendas_por_modelo)
+
+print("\nVENDAS POR FORMA DE PAGAMENTO")
+print("-------------------")
+print(vendas_pagamento)
